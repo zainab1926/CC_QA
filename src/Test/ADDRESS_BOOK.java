36bd99a1bece0a48dce9545efe5c40402f4e80d3
Mahesh <mahesh.nvs@royalcyber.com>
2017-09-16 03:15:13 -0500

    new changes for testing

--- src/Test/ADDRESS_BOOK.java
+++ src/Test/ADDRESS_BOOK.java
@@ -24,6 +24,7 @@ public class ADDRESS_BOOK extends Browser
 	
 	public void first()throws Exception
 	{
+		//commenting for Test
 		chkLogin("zzz@gmail.com","zainab123");
 		click("xpath=//*[@id='Header_GlobalLogin_loggedInDropdown']/div/div/div/div[2]/div[1]/div[2]/a"); //my acc
 	}
