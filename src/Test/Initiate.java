5abb27e72a80fe96393993aa93ffdc62a7136f67
Mahesh <mahesh.nvs@royalcyber.com>
2017-10-11 13:43:44 -0500

    DA-2 TEsting Jenkins CI

--- src/Test/Initiate.java
+++ src/Test/Initiate.java
@@ -40,7 +40,7 @@ public class Initiate {
 		extent = Report.GetExtent();
 //		System.setProperty("webdriver.gecko.driver", "C:\\CI_CD_CT\\Browser_Executables\\geckodriver.exe");
 //		driver = new FirefoxDriver();
-		//Adding New content for Testing Again
+		//TEsting Jenkins builds  for checking purpose
 		
 	}
 	@SuppressWarnings("static-access")
